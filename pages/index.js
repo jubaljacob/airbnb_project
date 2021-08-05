49fc4232fe257da4f697ac89ad939efcbb8d557c
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-05 14:10:46 +0530

    Update index.js
    header

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Jubal Airbnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       
-      <h1> Lets build airbnb</h1>
+      <Header />
+
+
+      {/* Banner */}
+      
 
       
     </div>
