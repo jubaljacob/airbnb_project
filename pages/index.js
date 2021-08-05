ae634445d80ffb3bbd3a0d6b77127cb903cd0f16
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-05 23:18:55 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import SmallCard from '../components/SmallCard';
 
-export default function Home() {
+export default function Home({ exploreData }) {
+ 
   return (
     <div className="">
       <Head>
@@ -14,6 +16,23 @@ export default function Home() {
       <Header />
       <Banner />
 
+      <main className="max-w-7xl mx-auto px-8 sm:px-16 ">
+        {/* mx-auto(central always )max-w(keeps it at that amximum width) */}
+        <section className="pt-6 ">
+          <h2 className="text-4xl font-semibold pd-5">Explore Nearby</h2>
+
+          {/* Pull data from a server --API endpoints */}
+          {exploreData?.map(({img, distance, location}) => (
+            <SmallCard
+            key={img}
+            img={img}
+            distance={distance}
+            location={location}
+            />
+          ))}
+        </section>
+      </main>
+
 
       {/* Banner */}
       
@@ -21,4 +40,17 @@ export default function Home() {
       
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp"). 
+  then(
+    (res) => res.json()
+  );
+
+  return{
+      props: {
+        exploreData: exploreData
+      }
+  }
 }
