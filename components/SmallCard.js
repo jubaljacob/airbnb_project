c20a0e477615bac29cc3f4d851bec2e78b7c8443
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-06 02:07:14 +0530

    Update SmallCard.js

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
-function SmallCard(img, location, distance) {
+function SmallCard({img, location, distance}) {
     return (
         <div>
             {/* Left */}
-            <div className="relative h-16 w-16">
+            <div className="relative h-16 w-16 ">
                 <Image src={img} layout="fill"/> 
             </div>
 
