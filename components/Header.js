d95a309cc33517584968915b3428ed3604493af3
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-05 16:32:14 +0530

    Update Header.js
    header finished

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid"
+import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UserIcon, } from "@heroicons/react/solid"
 
 
 function Header() {
@@ -18,12 +18,21 @@ function Header() {
 
             {/* Middle -- Search*/}
 
-            <div className="flex items-center border-2 rounded-full py-2 ">
-                <input type="text" placeholder="Type in your search"/>
-                <SearchIcon className=" hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer  "/>
+            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+                <input className=" flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Type in your search"/>
+                <SearchIcon className=" hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer  md:mx-2"/>
             </div>
             {/* Riget */}
-            <div></div>
+            <div className="flex items-center space-x-4 justify-end text-gray-500">  
+                                                        {/* above items-center = centrially alines in the flex which makes it in rows spacex4 gives a pd of 4 to all the x childern  */}
+                <p className="hidden md:inline cursor-pointer">Become a host</p>
+                <GlobeAltIcon className=" h-6 cursor-pointer"/>
+
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+                    <MenuIcon className="h-6"/>
+                    <UserCircleIcon className="h-6"/>
+                </div>
+            </div>
             
             
         </header>
