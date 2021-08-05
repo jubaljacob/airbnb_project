fa6379d7955fa17adec7b4d37fe3fec64dbc1b93
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-05 23:18:53 +0530

    Create SmallCard.js

---
+++ components/SmallCard.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+function SmallCard(img, location, distance) {
+    return (
+        <div>
+            {/* Left */}
+            <div className="relative h-16 w-16">
+                <Image src={img} layout="fill"/> 
+            </div>
+
+
+
+
+            {/* Right */}
+        </div>
+    )
+}
+
+export default SmallCard
