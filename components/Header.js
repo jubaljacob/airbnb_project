9fa0b4c2751811f04f577784263546f8774a2709
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-06 16:00:37 +0530

    Update Header.js
    sticky header

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UserIcon, } from "@
 
 function Header() {
     return (
-        <header className="stickey top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
 
             {/* left */}
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
