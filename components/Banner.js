1cc340f5e0ba775cb70a58ea57a891da84c4dd82
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-07 00:10:47 +0530

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 function Banner() {
     return (
-        <div className="relative h-[300px] sm:h-[400px] lg:h-[625px] xl:h-[800] 2xl:h-[900px] ">
+        <div className=" relative h-[300px] sm:h-[400px] lg:h-[625px] xl:h-[800] 2xl:h-[900px] ">
             <Image src="https://links.papareact.com/0fm" layout="fill"
             objectFit="cover"
             />
@@ -11,7 +11,7 @@ function Banner() {
                 <p className="text-sm sm:text-lg">
                     Not sure where to go ? perfect!
                 </p>
-                <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 duration-150" >I'm flexible</button>
+                <button className=" text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 duration-150" >I'm flexible</button>
             </div>
 
 
