09a949c2ade3d09f808da42f437c0a2653929721
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-06 18:56:29 +0530

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -8,7 +8,7 @@ function Banner() {
 
             <div className="absolute top-1/2 w-full text-center">
                 <p className="text-sm sm:text-lg">
-                    Not sure whaere to do??
+                    Not sure what to do??
                 </p>
                 <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 duration-150" >I'm flexible</button>
             </div>
