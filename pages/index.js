bfd7d4cab65f510e9406fc34b8b7f385c6f3148e
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-06 16:01:12 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import LargeCard from '../components/LargeCard';
 import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
@@ -17,7 +18,7 @@ export default function Home({ exploreData, cardsData }) {
       <Header />
       <Banner />
 
-      <main className="max-w-7xl mx-auto px-8 sm:px-16 ">
+      <main className="max-w-7xl mt-10 mb-10 mx-auto px-8 sm:px-16 bg-white shadow-lg rounded-xl ">
         {/* mx-auto(central always )max-w(keeps it at that amximum width) */}
         <section className="pt-6 ">
           <h2 className="text-4xl font-semibold pd-5">Explore Nearby</h2>
@@ -25,7 +26,7 @@ export default function Home({ exploreData, cardsData }) {
           {/* Pull data from a server --API endpoints */}
 
 
-          
+
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {exploreData?.map(({img, distance, location}) => (
               <SmallCard
@@ -43,16 +44,27 @@ export default function Home({ exploreData, cardsData }) {
         <section>
 
               <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
-              {cardsData?.map(({img, title}) =>(
-                <MediumCard key={img} img={img} title={title}/>
+              <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+                {cardsData?.map(({img, title}) =>(
+                  <MediumCard key={img} img={img} title={title}/>
 
-              ))}
+                ))}
 
+              </div>
+              
         </section>
+
+        <div></div>
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Whishlists curated by Airbnb."
+          buttonText="Get Inspired"
+        />
       </main>
 
 
-      {/* Banner */}
+      
       
 
       
