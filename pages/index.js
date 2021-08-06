e10e2fff6d77dd8b957f546966abaeae8f0c6aac
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-06 16:34:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
+import Footer from '../components/Footer';
 import Header from '../components/Header'
 import LargeCard from '../components/LargeCard';
 import MediumCard from '../components/MediumCard';
@@ -63,9 +64,8 @@ export default function Home({ exploreData, cardsData }) {
         />
       </main>
 
-
       
-      
+      <Footer />
 
       
     </div>
