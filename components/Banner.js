913a67da62677a6556e95354238a2eea266fcd2c
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-06 19:28:37 +0530

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -5,10 +5,11 @@ function Banner() {
             <Image src="https://links.papareact.com/0fm" layout="fill"
             objectFit="cover"
             />
+            <img src="public\hero.jpg" alt="" />
 
             <div className="absolute top-1/2 w-full text-center">
                 <p className="text-sm sm:text-lg">
-                    Not sure what to do??
+                    Not sure where to go ? perfect!
                 </p>
                 <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 duration-150" >I'm flexible</button>
             </div>
