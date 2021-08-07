5c6b84a106b64a56bb976bcd348f32ee959f48aa
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-07 23:04:52 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
     images:{
         domains:["links.papareact.com"],
     },
+     env: {
+            mapbox_key :"pk.eyJ1Ijoibm90bWVtYW4iLCJhIjoiY2tzMXpuMDV3MXpqYzJucHNnMXltYjdueiJ9.efsJZC7AvNPWyiIGFaCSWA"
+    },
 };
+
