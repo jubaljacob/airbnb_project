60793a7ef5b18efd18fefd7a1bd78a699daa02c7
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-11 21:13:41 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -65,7 +65,7 @@ function Header({ placeholder }) {
 
             {/* Middle -- Search*/}
 
-            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+            
                 <input 
                 value={searchInput}
                 onChange={(e) => setSearchInput(e.target.value)}
